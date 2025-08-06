97773f7a09183e5366858befdaacf0e8892adabe
ankando <i@ity.moe>
2025-08-07 00:23:56 +0800

    Update scrape-itch-downloads.js

--- scrape-itch-downloads.js
+++ scrape-itch-downloads.js
@@ -2,7 +2,11 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
   const page = await browser.newPage();
 
   console.log('Opening itch page...');
@@ -12,11 +16,10 @@ const fs = require('fs');
   await page.waitForSelector('.upload_list .upload');
 
   const links = await page.$$eval('.upload_list .upload a.button', anchors =>
-    anchors.map(a => a.href).filter(href => href.startsWith('https://')))
-  
+    anchors.map(a => a.href).filter(href => href.startsWith('https://')));
+
   console.log('Download links:', links);
 
-  // 保存成一个 Bash 下载脚本
   fs.writeFileSync('download.sh',
     links.map((link, i) => `curl -L "${link}" -o downloads/file${i}`).join('\n'),
     'utf-8'
